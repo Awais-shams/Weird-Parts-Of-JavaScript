5122f9050c831fbc136c3541efccae1c9b68f8aa
Awais Shams <82948178+Awais-shams@users.noreply.github.com>
2022-07-21 19:00:37 +0500

    some more javascript fundamentals

--- testing.js
+++ testing.js
@@ -4,12 +4,89 @@
 
 // var b=7;
 
-check();
+// check();
 
 
-var awais=function check(){
+// var awais=function check(){
     
-    var c=6;
+//     var c=6;
 
-    console.log(c);
+//     console.log(c);
+// }
+
+// {
+//     let a=6;
+
+//     const b=7;
+    
+//     var c=8;
+    
+//     var c=9;
+    
+//     console.log(a);
+// console.log(b);
+//     {
+//         var c=99;
+//         console.log(a)
+//     }
+// }
+
+
+
+// console.log(c);
+
+
+
+// // Actual Concepts of Callbacks
+
+// function doSomething(call){
+//     for(let i=0 ; i<3 ; i++){
+//         console.log(i,"Anonymous Identity is Learning The Javascript");
+//     }
+//     let a=80;
+//     call(a);
+// }
+
+// doSomething(function(x){
+//     console.log(x,": This is callback");
+//     for(let i=0 ; i<5; i++){
+//         console.log("Functions are the heart of javascript");
+//     }
+// })
+
+
+
+// // what are closures
+
+
+// setTimeout(function a(){
+//     console.log("first async calll");
+// },1000)
+
+// setTimeout(function a(){
+//     console.log("second async calll");
+// },1000)
+
+// setTimeout(function a(){
+//     console.log("Third async calll");
+// },1000)
+
+// setTimeout(function a(){
+//     console.log("Fourth async calll");
+// },1000)
+
+
+function awais(callback){
+    let a=b=7;
+    let x=a+b;
+    callback(x)
 }
+
+
+function  foo(){
+     return awais(function(X){
+        console.log("Is this Really a Foo Function: ",X);
+    })
+}
+
+foo()
